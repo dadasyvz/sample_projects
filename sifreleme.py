a21461ab6f5c2dcffe09e5bfadcb4ef3a07137c0
yavuz koksal <48917695+dadasyvz@users.noreply.github.com>
2023-11-23 10:16:29 +0100

    Update sifreleme.py

--- sifreleme.py
+++ sifreleme.py
@@ -1,7 +1,7 @@
 # Şifreleme Uygulaması
 # Kullaniciya iki secenek sunarak orjinal metni sifreli metne ve sifreli metni orjinal metne donusturebilen bir program yazmanizi istiyoruz.
-# Sozlukler yardimi ile bir sifreleme algoritmasi olusturun ve kullanicidan alacaginiz inputu bu algoritma yoluyla sifreleyin
-# ve ekrana yazdirin. Kullanici daha sonra bu sifreli metni input olarak yazdiginda orjinal metne ulasabilsin.
+# Sozlukler yardimi ile bir sifreleme algoritmasi olusturun ve kullanicidan alacaginiz inputu bu algoritma yoluyla sifreleyin ve ekrana yazdirin.
+# Kullanici daha sonra bu sifreli metni input olarak yazdiginda orjinal metne ulasabilsin.
 
 
 # coding=utf-8
